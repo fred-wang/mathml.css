cac0797f7c2bee4a6127a9fd5be0d36344d090ec
Frédéric Wang <fred.wang@free.fr>
2014-04-14 11:29:13 +0200

    Use Kuma's offscreen CSS for the test div (in particular to avoid page reflow)

--- mspace.js
+++ mspace.js
@@ -11,8 +11,17 @@
         // First check whether the page contains any <math> element.
         namespaceURI = "http://www.w3.org/1998/Math/MathML";
         if (document.body.getElementsByTagNameNS(namespaceURI, "math")) {
-            // Verify the support for the <mspace> element.
+            // Create a div to store the test, using Kuma's "offscreen" CSS
             div = document.createElement("div");
+            div.style.border = "0";
+            div.style.clip = "rect(0 0 0 0)"
+            div.style.height = "1px";
+            div.style.margin = "-1px";
+            div.style.overflow = "hidden";
+            div.style.padding = "0";
+            div.style.position = "absolute";
+            div.style.width = "1px";
+            // Verify the support for the <mspace> element.
             div.innerHTML = "<math xmlns='" + namespaceURI + "'><mspace height='23px' width='77px'/></math>";
             document.body.appendChild(div);
             box = div.firstChild.firstChild.getBoundingClientRect();
